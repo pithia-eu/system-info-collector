612aace6b099fc21d75a94ea5a67ac9a9c4ab1f8
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-16 19:58:45 +0300

    Refactor data collection functions to use ssh_command.
    Replaced inline SSH command execution code in data collection functions with calls to the generic ssh_command function. Updated dictionary keys in items_to_collect_list for clarity and added a timestamp entry to the collected_info_dict.

--- source/collect/collect.py
+++ source/collect/collect.py
@@ -1,7 +1,7 @@
-import scp
 from source.log.logger import logger
 from source.utils.environment import get_env_variable
 from source.utils.ssh import create_ssh_client
+from source.utils.ssh import ssh_command
 
 
 def is_true(env_variable):
@@ -15,21 +15,47 @@ def collect_hostname(host_ssh_client, test):
     logger.debug('Collecting Hostname..')
     if test:
         return "test_hostname"
+    command = "hostname"
+    ssh_command_out = ssh_command(host_ssh_client,command,
+                        "Error while collecting hostname",
+                         "Hostname collected")
+    return ssh_command_out.strip()
 
 def collect_date(host_ssh_client, test):
     logger.debug('Collecting Date..')
     if test:
         return "test_date"
+    command = "date"
+    ssh_command_out = ssh_command(host_ssh_client, command,
+                                  "Error while collecting date",
+                                  "Date collected")
+    return ssh_command_out.strip()
 
 def collect_disks(host_ssh_client, test):
     logger.debug('Collecting Disk..')
     if test:
         return "test_disk"
+    command = "df -h"
+    ssh_command_out = ssh_command(host_ssh_client, command,
+                                  "Error while collecting OS disk space",
+                                  "OS disk space collected")
+    ssh_command_out_lines = ssh_command_out.splitlines()
+    for line in ssh_command_out_lines:
+        line_list = line.split(" ")
+        processed_line = [item for item in line_list if item != ""]
+        if '/' in processed_line:
+            return " ".join(processed_line)
 
 def collect_updates(host_ssh_client, test):
     logger.debug('Collecting Updates..')
     if test:
         return "test_update"
+    command = "apt list --upgradable"
+    ssh_command_out = ssh_command(host_ssh_client, command,
+                                  "Error while collecting available updates",
+                                  "Available updates collected")
+    ssh_command_out_lines = ssh_command_out.splitlines()
+    return ssh_command_out.split("\n")
 
 def collect_info(timestamp,
                  test=False):
@@ -43,14 +69,15 @@ def collect_info(timestamp,
     collect_date_bool = is_true(get_env_variable("COLLECT_DATE"))
     collect_disks_bool = is_true(get_env_variable("COLLECT_DISKS"))
     collect_updates_bool = is_true(get_env_variable("COLLECT_UPDATES"))
-    items_to_collect_list = [{"hostname":{"enabled":collect_hostname_bool, "function":collect_hostname}},
-                             {"date":{"enabled":collect_date_bool, "function":collect_date}},
-                             {"disks":{"enabled":collect_disks_bool, "function":collect_disks}},
-                             {"updates":{"enabled":collect_updates_bool, "function":collect_updates}}]
+    items_to_collect_list = [{"host_name":{"enabled":collect_hostname_bool, "function":collect_hostname}},
+                             {"host_date":{"enabled":collect_date_bool, "function":collect_date}},
+                             {"host_disks":{"enabled":collect_disks_bool, "function":collect_disks}},
+                             {"host_updates":{"enabled":collect_updates_bool, "function":collect_updates}}]
     ssh_host_list = list(set(ssh_hosts.split(','))) if ssh_hosts else []
     collected_info_dict = {}
     for host in ssh_host_list:
         collected_info_dict[host] = {}
+        collected_info_dict[host]["run_date"] = timestamp
         if test:
             logger.warning(f"Test model enabled - Collecting info for host: {host} disabled")
         else:
